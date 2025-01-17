66fe8bc56d69cc1f3867b33b2d1c17a7bddd003a
syariful umam <syarifulumam7@gmail.com>
2025-01-17 07:34:57 +0700

    add : winston logger, prisma service, validation service

---
+++ src/common/common.module.ts
@@ -0,0 +1,22 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import { PrismaService } from './prisma.service';
+import { ValidationService } from './validation.service';
+
+@Global()
+@Module({
+  imports: [
+    WinstonModule.forRoot({
+      format: winston.format.json(), // Mengatur format log dalam bentuk JSON
+      transports: [new winston.transports.Console()], // Menentukan transportasi log ke konsol.
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  providers: [PrismaService, ValidationService],
+  exports: [PrismaService, ValidationService],
+})
+export class CommonModule {}
