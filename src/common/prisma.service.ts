851ab6ee55bba3d76a418f20877f76ca40077771
syariful umam <syarifulumam7@gmail.com>
2025-01-19 14:53:11 +0700

    add : register user, error handling

--- src/common/prisma.service.ts
+++ src/common/prisma.service.ts
@@ -1,7 +1,7 @@
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { Prisma, PrismaClient } from '@prisma/client';
-import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 
 @Injectable()
 export class PrismaService
---
+++ src/user/user.service.ts
@@ -0,0 +1,47 @@
+import { HttpException, Inject, Injectable } from '@nestjs/common';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { PrismaService } from 'src/common/prisma.service';
+import { ValidationService } from 'src/common/validation.service';
+import { RegisterUserRequest, UserResponse } from 'src/model/user.model';
+import { Logger } from 'winston';
+import { UserValidation } from './user.validation';
+import * as bcrypt from 'bcrypt';
+@Injectable()
+export class UserService {
+  constructor(
+    private validationService: ValidationService,
+    @Inject(WINSTON_MODULE_PROVIDER) private logger: Logger,
+    private prismaService: PrismaService,
+  ) {}
+
+  async register(request: RegisterUserRequest): Promise<UserResponse> {
+    this.logger.info(`Register new user ${JSON.stringify(request)}`);
+
+    const registerUserRequest: RegisterUserRequest =
+      this.validationService.validate(UserValidation.REGISTER, request);
+
+    const totalUser = await this.prismaService.user.count({
+      where: {
+        username: registerUserRequest.username,
+      },
+    });
+
+    if (totalUser > 0) {
+      throw new HttpException('Username already exist', 400);
+    }
+
+    registerUserRequest.password = await bcrypt.hash(
+      registerUserRequest.password,
+      10,
+    );
+
+    const user = await this.prismaService.user.create({
+      data: registerUserRequest,
+    });
+
+    return {
+      username: user.username,
+      name: user.name,
+    };
+  }
+}
