be25b79c650b3ef1b24c1f9b048f909afe1f6bb8
syariful umam <syarifulumam7@gmail.com>
2025-01-22 08:08:03 +0700

    feat : add contact

---
+++ src/contact/contact.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ContactService } from './contact.service';
+import { Auth } from 'src/common/auth.decorator';
+import { User } from '@prisma/client';
+import { ContactResponse, CreateContactRequest } from 'src/model/contact.model';
+import { WebResponse } from 'src/model/web.model';
+
+@Controller('/api/contacts')
+export class ContactController {
+  constructor(private contactService: ContactService) {}
+
+  @Post()
+  async create(
+    @Auth() user: User,
+    @Body() request: CreateContactRequest,
+  ): Promise<WebResponse<ContactResponse>> {
+    const result = await this.contactService.create(user, request);
+    return {
+      data: result,
+    };
+  }
+}
