c4cfe9f9c6f31f64223911689d657eba594d25d4
syariful umam <syarifulumam7@gmail.com>
2025-01-25 16:38:52 +0700

    add : get address

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, Param, ParseIntPipe, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { AddressService } from './address.service';
 import { Auth } from 'src/common/auth.decorator';
 import { User } from '@prisma/client';
-import { AddressResponse, CreateAddressRequest } from 'src/model/address.model';
+import {
+  AddressResponse,
+  CreateAddressRequest,
+  GetAddressRequest,
+} from 'src/model/address.model';
 import { WebResponse } from 'src/model/web.model';
 
 @Controller('/api/contacts/:contactId/addresses')
@@ -17,6 +28,20 @@ export class AddressController {
   ): Promise<WebResponse<AddressResponse>> {
     request.contact_id = contactId;
     const result = await this.addressService.create(user, request);
+    return { data: result };
+  }
+
+  @Get(':addressId')
+  async get(
+    @Auth() user: User,
+    @Param('contactId', ParseIntPipe) contactId: number,
+    @Param('addressId', ParseIntPipe) addressId: number,
+  ): Promise<WebResponse<AddressResponse>> {
+    const request: GetAddressRequest = {
+      contact_id: contactId,
+      address_id: addressId,
+    };
+    const result = await this.addressService.get(user, request);
     return { data: result };
   }
 }
--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -1,9 +1,13 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, Inject, Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { PrismaService } from 'src/common/prisma.service';
 import { ValidationService } from 'src/common/validation.service';
-import { AddressResponse, CreateAddressRequest } from 'src/model/address.model';
+import {
+  AddressResponse,
+  CreateAddressRequest,
+  GetAddressRequest,
+} from 'src/model/address.model';
 import { Logger } from 'winston';
 import { AddressValidation } from './address.validation';
 import { ContactService } from 'src/contact/contact.service';
@@ -37,6 +41,38 @@ export class AddressService {
     const address = await this.prismaService.address.create({
       data: createRequest,
     });
+
+    return {
+      id: address.id,
+      street: address.street,
+      city: address.city,
+      province: address.province,
+      country: address.country,
+      postal_code: address.postal_code,
+    };
+  }
+
+  async get(user: User, request: GetAddressRequest): Promise<AddressResponse> {
+    const getRequest: GetAddressRequest = this.validationService.validate(
+      AddressValidation.GET,
+      request,
+    );
+
+    await this.contactService.checkContactMustExists(
+      user.username,
+      getRequest.contact_id,
+    );
+
+    const address = await this.prismaService.address.findFirst({
+      where: {
+        id: getRequest.address_id,
+        contact_id: getRequest.contact_id,
+      },
+    });
+
+    if (!address) {
+      throw new HttpException('Address not found', 404);
+    }
 
     return {
       id: address.id,
